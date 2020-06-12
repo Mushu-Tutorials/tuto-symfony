21feb05d0be59b65e039e31f81f72f4658b234e2
Mushu <dragon.mushu@hotmail.com>
2020-06-12 18:24:49 +0200

    add delete entity

--- symfony/grafikart/src/Controller/Admin/AdminPropertyController.php
+++ symfony/grafikart/src/Controller/Admin/AdminPropertyController.php
@@ -8,6 +8,7 @@ use App\Repository\PropertyRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminPropertyController extends AbstractController
@@ -27,7 +28,7 @@ class AdminPropertyController extends AbstractController
 
   /**
    * @Route("/admin/property/new", name="admin_property_new")
-   * @Route("/admin/property/{id}", name="admin_property_edit")
+   * @Route("/admin/property/{id}", name="admin_property_edit", methods="GET|POST")
    */
   public function form(Property $property = null, Request $request, EntityManagerInterface $manager)
   {
@@ -35,10 +36,10 @@ class AdminPropertyController extends AbstractController
       $property = new Property;
     }
 
-		/**
-		 * Appel de la classe ArticleType pour générer le formulaire
-		 * Prérequis : Avoir exécuté la commande `php bin/console make:form`
-		 */
+    /**
+     * Appel de la classe PropertyType pour générer le formulaire
+     * Prérequis : Avoir exécuté la commande `php bin/console make:form`
+     */
     $form = $this->createForm(PropertyType::class, $property);
 
     /**
@@ -62,7 +63,34 @@ class AdminPropertyController extends AbstractController
     return $this->render('admin/create.html.twig', [
       'property' => $property,
       'form' => $form->createView(),
-			'editMode' => $property->getId() !== null,
-      ]);
+      'editMode' => $property->getId() !== null,
+    ]);
+  }
+
+  /**
+   * Tuto : https://youtu.be/6Ryu7-VSV5k?t=1593
+   * 
+   * @Route("/admin/property/{id}", name="admin_property_delete", methods="DELETE")
+   */
+  public function delete(Property $property = null, Request $request, EntityManagerInterface $manager)
+  {
+    /**
+     * Pour ajouter de la sécurité sur la suppression d'un bien,
+     * On ajoute une gestion des TOKENS CSRF
+     * 
+     * Sur la vue, on ajoute un champ INPUT HIDDEN (<input type="hidden" name="_token" value="{{ csrf_token('delete' ~ property.id) }}">)
+     * Avec un token généré en fonction de l'objet choisi
+     * 
+     * Dans le Controller, on ajoute le code ci-dessous
+     *   isCsrfTokenValid('valeur du token', 'récupération du name _token')
+     */
+    if ($this->isCsrfTokenValid('delete' . $property->getId(), $request->get('_token'))) {
+      $manager->remove($property);
+      $manager->flush();
+
+      // return new Response('Suppression');
+    }
+
+    return $this->redirectToRoute('admin_property_index');
   }
 }
